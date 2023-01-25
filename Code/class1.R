488eb6b0651e959f4943da4411d79ac7c72692eb
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-01-25 10:49:43 +0530

    making matrices in R

--- Code/class1.R
+++ Code/class1.R
@@ -1,3 +1,4 @@
+# 25 January 2023
 a = c(1:5)
 b = c(2,3,4,5,7)
 c = c('CHE')
@@ -7,3 +8,17 @@ typeof(a)
 
 cbind(a,b)
 cbind.data.frame(input = a, output = b)
+
+# in case of a figure margins too large error, you make the plot area larger than initially
+# this is when RStudio is unable to render the plot in the Plot Window
+
+m = matrix(c(1,2,3, 11, 12, 13), 
+           nrow = 2, ncol = 3, 
+           byrow = TRUE, 
+           dimnames = list(
+                        c("Data 1", "Data 2"), 
+                        c("C1", "C2", "C3"))
+           )
+
+
+
