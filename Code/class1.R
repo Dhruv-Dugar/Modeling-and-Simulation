5307ba44a7aaae8c18a04db2cfa35b4a8086c29a
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-01-27 10:28:29 +0530

    exporting selected data to a new excel file

--- Code/class1.R
+++ Code/class1.R
@@ -1,36 +1,53 @@
-# 25 January 2023
-a = c(1:5)
-b = c(2,3,4,5,7)
-c = c('CHE')
+# # 25 January 2023
+# a = c(1:5)
+# b = c(2,3,4,5,7)
+# c = c('CHE')
+# 
+# typeof(c)
+# typeof(a)
+# 
+# cbind(a,b)
+# cbind.data.frame(input = a, output = b)
+# 
+# # in case of a figure margins too large error, you make the plot area larger than initially
+# # this is when RStudio is unable to render the plot in the Plot Window
+# 
+# m = matrix(c(1,2,3, 11, 12, 13), 
+#            nrow = 2, ncol = 3, 
+#            byrow = TRUE, 
+#            dimnames = list(
+#                         c("Data 1", "Data 2"), 
+#                         c("C1", "C2", "C3"))
+#            )
+# 
+# 
+# # common statistical terms, and their usage on a vector
+# hist(a)
+# boxplot(a)
+# mean(a)
+# median(a)
+# sd(a)
+# 
+# # Regression analysis
+# 
+# model1 = lm(b~a)
+# anova(model1)
 
-typeof(c)
-typeof(a)
 
-cbind(a,b)
-cbind.data.frame(input = a, output = b)
+# Importing data from excel file
 
-# in case of a figure margins too large error, you make the plot area larger than initially
-# this is when RStudio is unable to render the plot in the Plot Window
-
-m = matrix(c(1,2,3, 11, 12, 13), 
-           nrow = 2, ncol = 3, 
-           byrow = TRUE, 
-           dimnames = list(
-                        c("Data 1", "Data 2"), 
-                        c("C1", "C2", "C3"))
-           )
+library(readxl)
+Book1 <- read_excel("ModSim/R/Data/Coffee_domestic_consumption.xlsx")
+View(Book1)
 
 
-# common statistical terms, and their usage on a vector
-hist(a)
-boxplot(a)
-mean(a)
-median(a)
-sd(a)
+Book1[6:10, 4:5]
+Book1[2:10, 6:12]
 
-# Regression analysis
+install.packages("writexl")
 
-model1 = lm(b~a)
-anova(model1)
+library(writexl)
+new_data = Book1[1:4, 1:5]
+write_xlsx(new_data, "ModSim/R/Data/New_data.xlsx")
 
 
