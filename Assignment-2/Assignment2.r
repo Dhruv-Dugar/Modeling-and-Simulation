f9ae8da5b0bb85969b30c1981d9a93a4c14b0ed0
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-04-09 22:02:42 +0530

    changed response variable to NOX as correlation is better

--- Assignment-2/Assignment2.r
+++ Assignment-2/Assignment2.r
@@ -32,7 +32,7 @@ pairs(data)
 # making a linear model for the prediction of relative humidity
 # with all the rest of the data points as the inputs for the model
 
-model <- lm(data$RH ~ data$CO_GT + data$PT08_S1_CO + data$C6H6_GT + data$PT08_S2_NMHC + data$NOX_GT + data$PT08_S3_NOX + data$NO2_GT + data$PT08_S4_NO2 + data$PT08_S5_O3 + data$T + data$AH + data$HOUR + data$MONTH) # nolint: line_length_linter.
+model <- lm(data$PT08_S3_NOX ~ data$CO_GT + data$PT08_S1_CO + data$C6H6_GT + data$PT08_S2_NMHC + data$RH + data$PT08_S3_NOX + data$NO2_GT + data$PT08_S4_NO2 + data$PT08_S5_O3 + data$T + data$AH + data$HOUR + data$MONTH) # nolint: line_length_linter.
 
 summary(model)
 plot(model)
@@ -41,11 +41,11 @@ anova(model)
 
 predict.lm(model)
 
-plot(data$RH, predict.lm(model))
+plot(data$PT08_S3_NOX, predict.lm(model))
 coef(model)
 
-rmse(predict.lm(model), data$RH)
-mape(predict.lm(model), data$RH)
+rmse(predict.lm(model), data$PT08_S3_NOX)
+mape(predict.lm(model), data$PT08_S3_NOX)
 
 # Similary we can make a non linear model provided we have the data about
 # the coefficients and what power and functions
