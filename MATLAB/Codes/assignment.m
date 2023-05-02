405153af9b6b75f5f21d16799f482cec3797270e
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-05-03 00:48:21 +0530

    solved using ode45

--- MATLAB/Codes/assignment.m
+++ MATLAB/Codes/assignment.m
@@ -3,22 +3,11 @@
 % T(z=0) = 313
 % z(max) = 0.05
 % Number of nodes = 10
-
 L = 0.05;
-N = 11;
-h = L/(N-1);
+N = 6;
+zspan = linspace(0 , L, N);
 
-% domain
-x = linspace(0, L, N);
+t0 = 40;
 
-% differential equation
-
-f = @(x) -400;
-
-A = zeros(N, N);
-A(1,1) = 50;
-
-
-for i = 2:N-1
-    A(i, i-1) = 1/h^2;
-    A(i)
+[t,z] = ode45(@(t,z) -400, zspan, t0);
+plot(t,z,"-o");
