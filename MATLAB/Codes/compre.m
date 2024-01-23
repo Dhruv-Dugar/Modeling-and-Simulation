62eed17d5c71743c278b5ddb9ea0462d3225d232
Dhurv Dugar <dhruvdugar02@gmail.com>
2024-01-23 19:29:53 +0530

    uploaded all data regarding modsim

--- MATLAB/Codes/compre.m
+++ MATLAB/Codes/compre.m
@@ -1,23 +1,51 @@
+% transient- heat conduction model
+nx=10;
+nt=3;
+x0= 0;
+xf= 1;
+t0=0;
+tf= 0.2;
+dx= (xf-x0)/(nx-1);
+dt= (tf-t0)/(nt-1);
+x= x0:dx:xf;
+t= t0:dt:tf;
 
-x = linspace(1, 10, 10);
+s= dt/dx^2;
 
-y1 = x.^3 + 3*x + 5;
-y2 = 5*x + 1;
+% boundary condition
 
-figure(1);
-plot(x, y1, "b-", "LineWidth",2);
-hold("on");
-plot(x, y2, "b-", "LineWidth",2);
+T= zeros(nx, nt);
+T= zeros(nx);
 
+%  T(1)=100;
+%  T(3)=30;
 
+% initial condition
+T(:,1)= sin(pi*x);
 
-% for 1:10
+% T0= 1;
+% T(:,1)= T0;
+
+for j= 1:nt-1
+    for i= 2:nx-1
+        T(i, j+1)= s*T(i-1, j)+(1-2*s)*T(i,j)+s*T(i+1,j);
+
+    end
+end
+
+%     for i= 2:nx-1
+%         T(i)= s*T(i-1)+(1-2*s)*T(i)+s*T(i+1);
 % 
-% function f = y1(x)
-%     f = x^3 + 3*x + 5;
-% end
-% 
-% 
-% function f = y2(x)
-%     f = 5*x + 1;
-% end
+%     end
+% % 
+%  plot(x,T);
+% plot(t,T);
+
+figure()
+contourf(T,200,'linecolor', 'non')
+xlabel('x')
+ylabel('t')
+title('Numerical solution (Transient Model)')
+colormap(jet(256))
+colorbar
+caxis([0,1])
